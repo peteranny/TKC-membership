d9eb68fc20eb0ad69f8021528c82487e3bd29e36
peteranny <peteranny2@gmail.com>
2017-01-15 18:01:14 +0800

    logged_in

--- public/js/index.js
+++ public/js/index.js
@@ -1,15 +1,30 @@
+var config = null;
 function init(){
-    $.getJSON("config.json", function(config) {
-        log(config)
+    $.getJSON('config.json', function(json) {
+        config = json;
         runGoogleAuth(
             config.api.client_id,
             config.api.scopes,
             true
         ).then(function(){
-            log('Authcheck passed')
+            log('Authcheck passed');
+            run();
         }).catch(function(err){
-            log('Error in init: ', err)
-        });//.then(showMain, hideMain);
+            log('Error in init: ', err);
+        });
     });
 }
 
+function initClick(){
+    runGoogleAuth(
+        config.api.client_id,
+        config.api.scopes,
+        false
+    ).then(function(){
+        run();
+    });
+}
+
+function run(){
+    vm.logged_in = true;
+}
