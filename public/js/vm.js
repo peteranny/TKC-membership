75f0152a6960eea0adcd92c4b3615d4f9240cb63
peteranny <peteranny2@gmail.com>
2017-01-15 19:46:40 +0800

    fix search bar

--- public/js/vm.js
+++ public/js/vm.js
@@ -2,5 +2,23 @@ var vm = new Vue({
     el: '#main',
     data:{
         logged_in: false,
+        query: '',
+        filters:[],
+    },
+    computed:{
+        unsolved_members: function(){
+            return [];
+        },
+        isSolved: function(){
+            return this.unsolved_members.length == 0;
+        },
+    },
+    methods:{
+        copyList: function(){
+            log('copy list');
+        },
+        gotoConfig: function(){
+            log('goto config');
+        },
     },
 });
