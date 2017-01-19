523e65ed6a96827ecc32e4e6d5ed35cb2b2bb744
peteranny <peteranny2@gmail.com>
2017-01-20 01:55:45 +0800

    save config

--- server.js
+++ server.js
@@ -1,33 +1,41 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 
 app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-var server = app.listen(app.get('port'), function() {
+const server = app.listen(app.get('port'), function() {
     console.log('Node app is running on port', app.get('port'));
 });
 
 // socket.io
 
-var socketIO = require('socket.io');
-var io = socketIO.listen(server);
-var fs = require('fs');
-var Promise = require('bluebird');
-var configPath = "./public/config.json";
+const socketIO = require('socket.io');
+const io = socketIO.listen(server);
+const fs = require('fs');
+const configPath = './public/config.json';
+
+function log(){
+    const msg = Array.from(arguments).reduce((a,b) => a + ' ' + (
+        b instanceof Error ? b.toString():
+        typeof b === 'object' ? JSON.stringify(b, null, 2):
+        b
+    ), '');
+    console.log(`${new Date()} ${msg}`);
+}
 
 io.on('connection', function(socket){
-    console.log('Receive connection');
+    log('Receive', 'CONNECTION');
     socket.on('disconnection', function(){
-        console.log('Receive disconnection');
+        log('Receive', 'DISCONNECTION');
     });
     socket.on('save', function(config){
-        console.log('Receive save');
-        var code = JSON.stringify(config, null, 2);
+        log('Receive', 'SAVE');
+        const code = JSON.stringify(config, null, 2);
         fs.writeFile(configPath, code, function(err){
-            if(err) console.log('Write :' + err);
-            else console.log('Write done');
+            if(err) log('ERROR', err);
+            else log('Done writing', configPath);
             socket.emit('saved', err);
         });
     });
